26e185a84a2f84f93af2beab88aff4739529ccba
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-10 16:36:51 +0100

    feat: stream redirect web runner

---
+++ README.md
@@ -0,0 +1,18 @@
+# Stream Redirect Web Runner
+
+Deploy this as a [Web Runner](https://app.osaas.io/dashboard/service/eyevinn-web-runner) and you have a stream redirector that is configurable using the [application config service](https://app.osaas.io/dashboard/service/eyevinn-app-config-svc).
+
+## Prerequisites
+
+- An account in Eyevinn Open Source Cloud with 2 services available in the plan.
+
+## Get started
+
+1. Fork this repository.
+2. Deploy this repository as a Web Runner in Eyevinn OSC. See [Web Runner documentation](https://docs.osaas.io/osaas.wiki/Service%3A-Web-Runner.html) for instructions.
+3. Go to [Application Config Service](https://app.osaas.io/dashboard/service/eyevinn-app-config-svc) and click on the instance called `streamredirector`
+4. Enter a name of your stream, e.g. `oscdemo` and a URL to the stream
+
+Obtain the URL to your web runner instance and open the URL `https://<webrunner>/play/oscdemo` and it will redirect and play the URL specified in the configuration.
+
+
---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { Context } from "@osaas/client-core";
+import { setupDatabase } from "@osaas/client-db";
+import { createEyevinnAppConfigSvcInstance, EyevinnAppConfigSvc, getEyevinnAppConfigSvcInstance } from "@osaas/client-services";
+import fastify from "fastify";
+
+async function initConfigService(ctx: Context): Promise<EyevinnAppConfigSvc> {
+  let instance = await getEyevinnAppConfigSvcInstance(ctx, 'streamredirector');
+  if (!instance) {
+    const redisUrl = await setupDatabase('valkey', 'streamredirector', { password: 'secret' });
+    instance = await createEyevinnAppConfigSvcInstance(ctx, {
+      name: 'streamredirector',
+      RedisUrl: redisUrl,
+    });
+  }
+  return instance;
+}
+
+async function readConfigVariable(service: EyevinnAppConfigSvc, key: string) {
+  const url = new URL(`/api/v1/config/${key}`, service.url);
+  const response = await fetch(url.toString(), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (!response.ok) {
+    return undefined;
+  }
+  const data = await response.json();
+  return data.value;
+}
+
+async function main() {
+  const ctx = new Context();
+  const server = fastify();
+  const configService = await initConfigService(ctx);
+  if (!configService) {
+    console.error('Failed to initialize config service');
+    return;
+  }
+
+  server.get('/', async (request, reply) => {
+    reply.send('Hello World');
+  });
+
+  server.get<{
+    Params: { id: string }
+  }>('/play/:id', async (request, reply) => {
+    try {
+      console.log(`Request to play ${request.params.id}`);
+      const value = await readConfigVariable(configService, `${request.params.id}`);
+      if (!value) {
+        reply.code(404).send('Not found');
+      }
+      reply.redirect(value);
+    } catch (err) {
+      console.error(err);
+      reply.code(500).send('Internal server error');
+    }
+  });
+
+  server.listen({ host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 8080 }, (err, address) => {
+    if (err) console.error(err);
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+main();
